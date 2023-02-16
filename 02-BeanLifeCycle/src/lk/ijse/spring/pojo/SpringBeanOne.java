cac4d0e87269236742769cc0cbfdd53bafb0afa9
Rashmiya <rashmiyarafeek1200@gmail.com>
2023-02-17 01:10:41 +0530

    implements beanNameAware and beanFactoryAware

--- 02-BeanLifeCycle/src/lk/ijse/spring/pojo/SpringBeanOne.java
+++ 02-BeanLifeCycle/src/lk/ijse/spring/pojo/SpringBeanOne.java
@@ -1,10 +1,24 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBeanOne {
+public class SpringBeanOne implements BeanNameAware, BeanFactoryAware {
     public SpringBeanOne(){
         System.out.println("Spring Bean One instantiated!");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Spring Bean One : Bean Name Aware");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Spring Bean One : Bean Factory Aware");
     }
 }
