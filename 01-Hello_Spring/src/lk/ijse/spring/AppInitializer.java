beade9a3919f0dd7ba2ef97df8b5cd76c2363799
Rashmiya <rashmiyarafeek1200@gmail.com>
2023-02-16 11:59:38 +0530

    check the reference address of a Bean

--- 01-Hello_Spring/src/lk/ijse/spring/AppInitializer.java
+++ 01-Hello_Spring/src/lk/ijse/spring/AppInitializer.java
@@ -4,6 +4,7 @@ import lk.ijse.spring.config.AppConfig;
 import lk.ijse.spring.pojo.SpringBean;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+/*desktop application ekata deela thiyena object eka - AnnotationConfigApplicationContext */
 public class AppInitializer {
     public static void main(String args[]){
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
@@ -11,8 +12,13 @@ public class AppInitializer {
         ctx.refresh();
 
         SpringBean bean = ctx.getBean(SpringBean.class);
-        bean.test();  /*return a method in a Bean class*/
-        System.out.println(bean);  /*object eke address eka return wenwa*/
+        SpringBean bean1 = ctx.getBean(SpringBean.class);
+      /*  bean.test();  *//*return a method in a Bean class*//*
+        System.out.println(bean);  *//*object eke address eka return wenwa*/
+
+        /*check same reference or not*/
+        System.out.println(bean);
+        System.out.println(bean1);
 
         ctx.close();
     }
