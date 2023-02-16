7d07a2dac34e1c3d34dd57d5f9065e9766c6825b
Rashmiya <rashmiyarafeek1200@gmail.com>
2023-02-17 01:02:09 +0530

    content updated

---
+++ 02-BeanLifeCycle/src/lk/ijse/spring/pojo/SpringBeanOne.java
@@ -0,0 +1,10 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBeanOne {
+    public SpringBeanOne(){
+        System.out.println("Spring Bean One instantiated!");
+    }
+}
