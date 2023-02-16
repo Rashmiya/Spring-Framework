63523dab965354e326a96d42af2885e603f2c354
Rashmiya <rashmiyarafeek1200@gmail.com>
2023-02-17 01:14:29 +0530

    implements ApplicationContextAware and initializingBean

--- 02-BeanLifeCycle/src/lk/ijse/spring/pojo/SpringBeanOne.java
+++ 02-BeanLifeCycle/src/lk/ijse/spring/pojo/SpringBeanOne.java
@@ -4,10 +4,13 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBeanOne implements BeanNameAware, BeanFactoryAware {
+public class SpringBeanOne implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean {
     public SpringBeanOne(){
         System.out.println("Spring Bean One instantiated!");
     }
@@ -20,5 +23,15 @@ public class SpringBeanOne implements BeanNameAware, BeanFactoryAware {
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
         System.out.println("Spring Bean One : Bean Factory Aware");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Spring Bean One : Application Context Aware");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Spring Bean One : initializing Bean : Bean Ready");
     }
 }
