6b838b411c31c02a0c7e5c18ad8a8eb5abdfb4a3
Rashmiya <rashmiyarafeek1200@gmail.com>
2023-02-21 20:59:00 +0530

    add DataSource configurations

--- Spring-Data-JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ Spring-Data-JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -2,17 +2,44 @@ package lk.ijse.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class JPAConfig {
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter jpaVendor){
         LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
 
-        /*add features*/
-
+        /*add Entity class location*/
+        bean.setPackagesToScan("lk.ijse.spring.entity");
+        /*add DataSource*/
+        bean.setDataSource(ds);
+        /*add VendorAdapt*/
+        bean.setJpaVendorAdapter(jpaVendor);
         return bean;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        // DriverManager dataSource only for testing
+        // for production level , we use DBC pool / Inbuilt pool
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql//localhost:3306/springjpa?createDatabaseIfNotExist=true");  /*in here database name is "springjpa"*/
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+
     }
 }
