9e9d610a0e32ece4a5fe112301730bd9ffa17b52
Rashmiya <rashmiyarafeek1200@gmail.com>
2023-02-22 12:11:39 +0530

    add transactionManager to JPA config class

--- Spring-Data-JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ Spring-Data-JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -3,9 +3,14 @@ package lk.ijse.spring.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
@@ -40,6 +45,16 @@ public class JPAConfig {
 
     @Bean
     public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter va = new HibernateJpaVendorAdapter();
+        va.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+        va.setDatabase(Database.MYSQL);
+        va.setGenerateDdl(true);
+        va.setShowSql(true);
+        return va;
+    }
 
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+        return new JpaTransactionManager(emf);
     }
 }
