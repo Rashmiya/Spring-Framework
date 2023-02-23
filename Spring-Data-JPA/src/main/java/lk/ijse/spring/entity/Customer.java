d2a6bccca95a114dcfa87a464e3f0fd5f1955f55
Rashmiya <rashmiyarafeek1200@gmail.com>
2023-02-23 20:07:23 +0530

    constructors,getter,setters,toString methods replaced by using lombok annotations

--- Spring-Data-JPA/src/main/java/lk/ijse/spring/entity/Customer.java
+++ Spring-Data-JPA/src/main/java/lk/ijse/spring/entity/Customer.java
@@ -1,8 +1,15 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 public class Customer {
     @Id  /*primary key - id*/
@@ -10,56 +17,4 @@ public class Customer {
     private String name;
     private String address;
     private double salary;
-
-    public Customer() {
-    }
-
-    public Customer(String id, String name, String address, double salary) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.salary = salary;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                ", salary=" + salary +
-                '}';
-    }
 }
--- Spring-Data-JPA/src/main/java/lk/ijse/spring/entity/Item.java
+++ Spring-Data-JPA/src/main/java/lk/ijse/spring/entity/Item.java
@@ -1,8 +1,15 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 public class Item {
     @Id  /*primary key - code*/
@@ -10,56 +17,4 @@ public class Item {
     private String name;
     private int qtyOnHand;
     private double unitPrice;
-
-    public Item() {
-    }
-
-    public Item(String code, String name, int qtyOnHand, double unitPrice) {
-        this.code = code;
-        this.name = name;
-        this.qtyOnHand = qtyOnHand;
-        this.unitPrice = unitPrice;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getQtyOnHand() {
-        return qtyOnHand;
-    }
-
-    public void setQtyOnHand(int qtyOnHand) {
-        this.qtyOnHand = qtyOnHand;
-    }
-
-    public double getUnitPrice() {
-        return unitPrice;
-    }
-
-    public void setUnitPrice(double unitPrice) {
-        this.unitPrice = unitPrice;
-    }
-
-    @Override
-    public String toString() {
-        return "Item{" +
-                "code='" + code + '\'' +
-                ", name='" + name + '\'' +
-                ", qtyOnHand=" + qtyOnHand +
-                ", unitPrice=" + unitPrice +
-                '}';
-    }
 }
