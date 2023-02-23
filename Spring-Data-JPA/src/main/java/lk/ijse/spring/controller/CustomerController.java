9346bbc7cb249a71f8608b200e1715fd00d952eb
Rashmiya <rashmiyarafeek1200@gmail.com>
2023-02-23 11:42:26 +0530

    updated crud operations with dao interfaces

--- Spring-Data-JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ Spring-Data-JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,12 +1,14 @@
 package lk.ijse.spring.controller;
 
 import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/customer")
@@ -20,7 +22,9 @@ public class CustomerController {
     public ResponseUtil saveCustomer(@ModelAttribute CustomerDTO dto){
         System.out.println(dto.toString());
 
-        repo.save();
+        /*create customer object and put dto objects into customerObject*/
+        Customer customer = new Customer(dto.getId(), dto.getName(), dto.getAddress(), dto.getSalary());
+        repo.save(customer);
 
         return new ResponseUtil(200,dto.toString()+"Successfully Added",null);
     }
@@ -28,23 +32,34 @@ public class CustomerController {
     @PutMapping
     public ResponseUtil updateCustomer(@RequestBody CustomerDTO dto){
         System.out.println(dto.toString());
+
+        Customer updateCustomer = new Customer(dto.getId(),dto.getName(), dto.getAddress(), dto.getSalary());
+        repo.save(updateCustomer);
+
         return new ResponseUtil(200,dto.toString()+"Customer Updated",null);
     }
 
     @DeleteMapping(params = "id")
     public ResponseUtil deleteCustomer(String id){
         System.out.println(id);
+
+        repo.deleteById(id);
+
         return new ResponseUtil(200,id+"Customer Deleted",null);
     }
 
     @GetMapping
     public ResponseUtil getAllCustomer(){
-        ArrayList<CustomerDTO> allCustomers = new ArrayList<>();
-        allCustomers.add(new CustomerDTO("C001","Rashmiya","Padukka",45000));
+        /*ArrayList<CustomerDTO> allCustomers = new ArrayList<>();*/
+        /* allCustomers.add(new CustomerDTO("C001","Rashmiya","Padukka",45000));
         allCustomers.add(new CustomerDTO("C002","Nipun","Colombo",34500));
         allCustomers.add(new CustomerDTO("C003","kamal","Nugegoda",54000));
         allCustomers.add(new CustomerDTO("C004","Sunil","Meepe",20000));
-        allCustomers.add(new CustomerDTO("C005","Jayanthi","Waththala",30000));
-        return new ResponseUtil(200,"Success",allCustomers);
+        allCustomers.add(new CustomerDTO("C005","Jayanthi","Waththala",30000));*/
+
+        /*return new ResponseUtil(200,"Success",allCustomers);*/
+
+        List<Customer> all = repo.findAll();
+        return new ResponseUtil(200,"Success",all);
     }
 }
