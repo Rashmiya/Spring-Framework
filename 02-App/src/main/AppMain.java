d6b87a977796b7f6e4bcacf0a17ba63172fb43b4
Rashmiya <rashmiyarafeek1200@gmail.com>
2023-06-19 16:25:32 +0530

    catch data from frontend using POST method

--- 02-App/src/main/AppMain.java
+++ 02-App/src/main/AppMain.java
@@ -36,6 +36,30 @@ public class AppMain extends HttpServlet {
        writer.write("<h1>"+description+"</h1>");
         writer.write("<h1>"+price+"</h1>");
         writer.write("<h1>"+qty+"</h1>");*/
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("do Post method Involked");
+
+        /*catch the user inputs*/
+        String item_name = req.getParameter("item_name");
+        String description = req.getParameter("description");
+        String price = req.getParameter("price");
+        String qty = req.getParameter("qty");
+
+        /*deliver a response*/
+        PrintWriter writer = resp.getWriter();
+        writer.write("<table border='1' width='100%'>");
+
+        writer.write("<thead>");
+        writer.write("<tr><th>Item Name</th><th>Description</th><th>Price</th><th>Qty</th></tr>");
+        writer.write("</thead>");
+
+        writer.write("<tbody>");
+        writer.write("<tr><th>"+item_name+"</th><th>"+description+"</th><th>"+price+"</th><th>"+qty+"</th></tr>");
+        writer.write("</tbody>");
+
+        writer.write("</table>");
     }
 }
